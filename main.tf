41b2a9d396e6ec48eddd2c8eb84842cdcb51ba81
RedaZenagui <gr_saberzenagui@esi.dz>
2023-10-05 09:46:55 +0000

    Partie 3

--- main.tf
+++ main.tf
@@ -34,14 +34,48 @@ resource "google_sql_user" "wordpress" {
    password = "ilovedevops"
 }
 
- #Création du bucket
- #resource "google_storage_bucket" "default" {
- #    name          = "devops-session6-tp" 
- #    location      = "EU"
- #    force_destroy = true
- #   versioning {
- #       enabled = true
- #   }     
- #}
+resource "google_cloud_run_service" "default" {
+name     = "serveur-wordpress"
+location = "us-central1"
 
+template {
+   spec {
+      containers {
+      image = "us-central1-docker.pkg.dev/projet-module-devops/website-tools/image-with-cloudbuild"
+      ports {
+          container_port = 80
+        }
+      }
+      
+   }
+
+   metadata {
+      annotations = {
+            "run.googleapis.com/cloudsql-instances" = "projet-module-devops:us-central1:main-instance"
+      }
+   }
+}
+
+traffic {
+   percent         = 100
+   latest_revision = true
+}
+}
+
+data "google_iam_policy" "noauth" {
+   binding {
+      role = "roles/run.invoker"
+      members = [
+         "allUsers",
+      ]
+   }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+   location    = google_cloud_run_service.default.location
+   project     = google_cloud_run_service.default.project
+   service     = google_cloud_run_service.default.name
+
+   policy_data = data.google_iam_policy.noauth.policy_data
+}
 
